2e357e205676fc857b38def43e8677cd80fd1ee6
Josh Sellers <joshuasellersd@gmail.com>
2019-09-21 22:35:37 -0400

    going back to original format
    the npm start works for this but reactstrap seems pretty shoddy

--- src/Components/app.js
+++ src/Components/app.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Example from './headerComponent/navBar.js';
+// import { render } from 'react-dom';
+import NavBar from './headerComponent/navBar.js';
+import HomePage from './pages/homePage'
+import Footer from './footerComponent/footer.js'
 
 class App extends Component {
 
     render() {
         return (
             <div>
-                <Example />
+                <NavBar />
+                <HomePage />
                 <a href="https://reactstrap.github.io/components/navbar/">reactstrap Navbar example</a>
+                <Footer />
             </div>
         );
     }
