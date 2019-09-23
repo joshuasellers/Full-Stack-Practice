23bc0daf3689273beab4092ec2748d6da7474ac8
Josh Sellers <joshuasellersd@gmail.com>
2019-09-23 19:30:26 -0400

    not done yet

--- src/Components/pages/application.js
+++ src/Components/pages/application.js
@@ -4,17 +4,22 @@ import './application.css';
 class Application extends Component {
     render() {
         return (
-            <div className="app-page">
-               <table className="tg">
-                    <tr>
-                        <th className="tg-tlxv"></th>
-                        <th className="tg-hayz"></th>
-                        <th className="tg-hayz"></th>
-                        <th className="tg-hayz"></th>
-                        <th className="tg-hayz"></th>
-                        <th className="tg-hayz"></th>
-                    </tr>
-                </table>
+            <div className={"app-page"}>
+                <span className={"item"}>
+                    <table className="tg">
+                        <tr>
+                            <th><input className="tg-tlxv" type="text" name="firstname" value="COMPANY"/></th>
+                            <th><input className="tg-hayz" type="text" name="firstname" value="POSITION"/></th>
+                            <th><input className="tg-hayz" type="text" name="firstname" value="LINK"/></th>
+                            <th><input className="tg-hayz" type="text" name="firstname" value="STATUS"/></th>
+                            <th><input className="tg-hayz" type="text" name="firstname" value="DATE APPLIED"/></th>
+                            <th><input className="tg-hayz" type="text" name="firstname" value="DATE CONTACTED"/></th>
+                        </tr>
+                    </table>
+                </span>
+                <span className={"item"}>
+                    <button type="button" className={"button-app"}>Add Row</button>
+                </span>
             </div>
 
         )
