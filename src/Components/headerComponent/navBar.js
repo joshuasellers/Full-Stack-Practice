05eb59641ff795478b66805bd7aeb7db07ab25a9
Josh Sellers <joshuasellersd@gmail.com>
2019-09-22 20:48:54 -0400

    all done with home page

--- src/Components/headerComponent/navBar.js
+++ src/Components/headerComponent/navBar.js
@@ -7,8 +7,8 @@ class NavBar extends Component {
         return (
             <ul>
                 <li><a className="active" href="#home">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="#applications">Applications</a></li>
+                <li><a href="#to-dos">To-Dos</a></li>
                 <li><a href="#about">About</a></li>
             </ul>
         )
