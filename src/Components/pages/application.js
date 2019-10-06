3fa51acdb81d3644bdfc743f6882481d22ec2eeb
Josh Sellers <joshuasellersd@gmail.com>
2019-10-05 22:34:35 -0400

    fricking apis man

--- src/Components/pages/application.js
+++ src/Components/pages/application.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './application.css';
 
 class Application extends Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+
+        const response = fetch('/api/form-submit-url', {
+            method: 'POST',
+            body: data,
+        });
+    }
+
     render() {
         return (
             <div className={"app-page"}>
                 <h2>New Job Application</h2>
                 <p>Add in the information related to your most recent job application</p>
                 <span className={"item"}>
-                    <form action="/action_page.php">
+                    <form onSubmit={this.handleSubmit}>
                         <fieldset>
                         <legend className={"legend-val"}>Personal information:</legend>
                         Company:<br/>
