393bb6686941088172c2ccee848d626fc86d101f
Josh Sellers <joshuasellersd@gmail.com>
2019-10-29 22:03:49 -0400

    trying somethings different

--- src/Components/pages/application.js
+++ src/Components/pages/application.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './application.css';
 import axios from "axios";
-
+import aws from 'aws-sdk';
+import {getSecretStuff} from '../../process';
 class Application extends Component {
     constructor(props) {
         super(props);
@@ -10,8 +11,6 @@ class Application extends Component {
             jobTitle: "",
             jobLink: "",
             dateApplied: "",
-            success: false,
-            url: "",
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -26,10 +25,6 @@ class Application extends Component {
 
         this.setState({
             [name]: value
-        });
-        this.setState({
-            success: false,
-            url : ""
         });
     }
 
@@ -59,42 +54,23 @@ class Application extends Component {
     }*/
 
     // Perform the upload
-    handleUpload(event){
-        let file = this.uploadInput.files[0];
+    handleSubmit(event){
         // Split the filename to get the name and type
-        let fileParts = this.uploadInput.files[0].name.split('.');
-        let fileName = fileParts[0];
-        let fileType = fileParts[1];
+        const entry = {
+            "companyName": this.state.companyName,
+            "jobTitle": this.state.jobTitle,
+            "jobLink": this.state.jobLink,
+            "dateApplied": this.state.dateApplied
+        };
         console.log("Preparing the upload");
-        axios.post("http://localhost:3001/sign_s3",{
-            fileName : fileName,
-            fileType : fileType
-        })
-            .then(response => {
-                var returnData = response.data.data.returnData;
-                var signedRequest = returnData.signedRequest;
-                var url = returnData.url;
-                this.setState({url: url});
-                console.log("Recieved a signed request " + signedRequest);
+        // Configure aws with your accessKeyId and your secretAccessKey
+        const things = getSecretStuff();
+        aws.config.update({
+            region: 'us-east-1', // Put your aws region here
+            accessKeyId: things.keyId,
+            secretAccessKey: things.secretKey
+        });
 
-                // Put the fileType in the headers for the upload
-                var options = {
-                    headers: {
-                        'Content-Type': fileType
-                    }
-                };
-                axios.put(signedRequest,file,options)
-                    .then(result => {
-                        console.log("Response from s3");
-                        this.setState({success: true});
-                    })
-                    .catch(error => {
-                        alert("ERROR " + JSON.stringify(error));
-                    })
-            })
-            .catch(error => {
-                alert(JSON.stringify(error));
-            })
     }
 
     render() {
